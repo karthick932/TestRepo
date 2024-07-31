8fcd31245a0e8aa9b1442278936bf0b6976544a8
Karthick Balu <karthick_ba@prod.hclpnp.com>
2024-07-31 13:18:50 +0530

    message

---
+++ APItest1/Logical/AddNumbers.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1722411771640"
+    createdUser="karthick_ba" id="-421d8e47:19107bd6768:-7ebf"
+    type="service_component_resource" updatedTimestamp="1722411771640"
+    updatedUser="karthick_ba" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
